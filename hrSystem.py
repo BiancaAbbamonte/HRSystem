fe8952a846df6519dbc2f176b41d90268383a441
Bianca Abbamonte Aveno <bianca.abbamonte@yahoo.com.br>
2024-08-18 16:59:36 -0300

    Add files via upload

---
+++ hrSystem.py
@@ -0,0 +1,34 @@
+# Design an HR system, where if two employees have a conflict, find the manager that can mediate the conflict
+# This manager must be in the reporting chain of both the employees.
+
+reporting_chain = {
+  'Alice': 'João',
+  'Amanda': 'João',
+  'Caio': 'Alice',
+  'Bianca': 'Amanda',
+  'João': 'Marcio',
+  'Marcio': 'Roberta'
+}
+
+def get_reporting_chain(employee, reporting_chain):
+  
+  chain = []
+  
+  while employee:
+      chain.append(employee)
+      employee = reporting_chain.get(employee)
+  return chain
+
+def common_manager(emp1, emp2, reporting_chain):
+  chain1 = get_reporting_chain(emp1, reporting_chain)
+  chain2 = get_reporting_chain(emp2, reporting_chain)
+  
+  for manager in chain1:
+    if manager in chain2:
+      return manager
+
+
+employee1 = 'Caio'
+employee2 = 'Bianca'
+the_common_manager = common_manager(employee1, employee2, reporting_chain)
+print(f"The common manager is {the_common_manager}.")
