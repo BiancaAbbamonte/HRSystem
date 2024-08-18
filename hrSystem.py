c14335b072ce49b000295e6f9ae1327bd0107563
Bianca Abbamonte Aveno <bianca.abbamonte@yahoo.com.br>
2024-08-18 17:00:09 -0300

    Update hrSystem.py

--- hrSystem.py
+++ hrSystem.py
@@ -1,6 +1,3 @@
-# Design an HR system, where if two employees have a conflict, find the manager that can mediate the conflict
-# This manager must be in the reporting chain of both the employees.
-
 reporting_chain = {
   'Alice': 'João',
   'Amanda': 'João',
